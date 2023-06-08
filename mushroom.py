759c1a140a957d918556c99060019eb602e17f20
Tejaswini <aswinivanimireddy@gmail.com>
2023-06-08 19:57:51 +0530

    updated code

--- mushroom.py
+++ mushroom.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from sklearn import metrics
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder,OneHotEncoder
